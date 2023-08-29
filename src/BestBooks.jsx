73b6aa4876bf542cfb6a02702f17a38e2e031b6e
Nika Skeba <67114318+nikaskeba@users.noreply.github.com>
2023-08-28 17:21:42 -0700

    fix carousel styling

--- src/BestBooks.jsx
+++ src/BestBooks.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Carousel from 'react-bootstrap/Carousel';
-
-import 'bootstrap/dist/js/bootstrap.bundle.min'; // Import Bootstrap JavaScript
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import './carousel.css';
 
 class BestBooks extends React.Component {
   constructor(props) {
@@ -38,23 +39,36 @@ class BestBooks extends React.Component {
   render() {
    return (
       <>
-        <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
-
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
+        <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
 
-
-        <ol>
-        {this.state.books.map((book, index) => (
-          <>
-            <li key={index}>{book.title}</li>
-            <ul>
-              <li>{book.description}</li>
-              <li>{book.status}</li>
-            </ul>
-          </>
-        ))}
-        </ol>
+    
+  <div id="bookCarousel" className="carousel slide" data-bs-ride="carousel">
+        <div className="carousel-inner">
+          {this.state.books.map((book, index) => (
+            <div className={`carousel-item ${index === 0 ? 'active' : ''}`} key={index}>
+              <ol class="list-group">
+                <li><p class="h4">{book.title}</p>
+                  <ul>
+                    <li class="list-group-item">{book.description}</li>
+                    <li class="list-group-item">{book.status}</li>
+                  </ul>
+                </li>
+              </ol>
+            </div>
+          ))}
+        </div>
+        <button className="carousel-control-prev" type="button" data-bs-target="#bookCarousel" data-bs-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button className="carousel-control-next" type="button" data-bs-target="#bookCarousel" data-bs-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
+  
 
       </>
     )
